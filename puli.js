6066eae7d917b33be32d7f7e07f83e5314c118e6
agiledineshreddy <126139844+agiledineshreddy@users.noreply.github.com>
2023-03-29 08:26:09 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ puli.js
@@ -0,0 +1,7 @@
+function indnum(){
+    if(parseInt(num.value/10000000000)==91){
+    document.getElementById("display").value="indian number";
+}
+else{
+    document.getElementById("display").value="not a indian number";}
+}
